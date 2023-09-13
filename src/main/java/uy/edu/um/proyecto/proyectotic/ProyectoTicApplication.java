04256845d427b013379f1ec29480b4c334f5da11
MatiRasnik <64052131+MatiRasnik@users.noreply.github.com>
2023-09-13 18:42:03 -0300

    push

---
+++ src/main/java/uy/edu/um/proyecto/proyectotic/ProyectoTicApplication.java
@@ -0,0 +1,14 @@
+package uy.edu.um.proyecto.proyectotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTicApplication.class, args);
+        System.out.println("Hola mundo");
+    }
+
+}
---
+++ src/test/java/uy/edu/um/proyecto/proyectotic/ProyectoTicApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.um.proyecto.proyectotic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
