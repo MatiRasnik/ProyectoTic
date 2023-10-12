f2e2653002903dd776e886ac0a6d71bd9ff46e53
MatiRasnik <64052131+MatiRasnik@users.noreply.github.com>
2023-10-12 11:01:21 -0300

    borrando la visual

--- src/main/java/uy/edu/um/proyecto/proyectotic/JavaFXMain.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/JavaFXMain.java
@@ -1,17 +1,9 @@
 package uy.edu.um.proyecto.proyectotic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import net.rgielen.fxweaver.core.FxWeaver;
-import uy.edu.um.proyecto.proyectotic.Controladores.creacionAerolineaController;
-import uy.edu.um.proyecto.proyectotic.Controladores.creacionEmpleadoAeropuertoController;
-import uy.edu.um.proyecto.proyectotic.Controladores.loginController;
-import uy.edu.um.proyecto.proyectotic.Controladores.vistaMenuAeropuertoController;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
@@ -28,14 +20,14 @@ public class JavaFXMain extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-		FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
+    public void start(Stage primaryStage){
+		/*FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
 		Parent root = fxWeaver.loadView(loginController.class);
     	Scene scene = new Scene(root);
     	primaryStage.setScene(scene);
     	primaryStage.show();
 
-
+		*/
 
 	}
 	@Override
--- src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/VentanasEmergentes.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/VentanasEmergentes.java
@@ -4,16 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import net.rgielen.fxweaver.core.FxWeaver;
-import uy.edu.um.proyecto.proyectotic.Controladores.errorEmergenteController;
-import uy.edu.um.proyecto.proyectotic.Controladores.loginController;
+
 
 @Component
 
@@ -29,12 +24,12 @@ public class VentanasEmergentes {
         popupStage.initModality(Modality.APPLICATION_MODAL);
         popupStage.initStyle(StageStyle.UTILITY);
         popupStage.setTitle("Popup Window");
-
+/*
         Parent root = fxWeaver.loadView(errorEmergenteController.class);
         fxWeaver.getBean(errorEmergenteController.class).setMessageError(mensaje);
     	Scene scene = new Scene(root);
         popupStage.setScene(scene);
-        popupStage.showAndWait();
+        popupStage.showAndWait();*/
     }
         
     
