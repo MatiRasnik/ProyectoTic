6abbd787b18948e90765dba52d57a2785af9c88d
geropo08 <84402339+geropo08@users.noreply.github.com>
2023-11-03 08:28:11 -0300

    ids

--- src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/Aviones/AvionesRepository.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/Aviones/AvionesRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import uy.edu.um.proyecto.proyectotic.Persistencia.Usuarios.Usuarios;
 
-public interface AvionesRepository extends JpaRepository<Aviones,Long>{
+public interface AvionesRepository extends JpaRepository<Aviones,String>{
     @Query("SELECT a FROM Aviones a WHERE a.matricula = ?1")
     Aviones findByMatricula(String matricula);
 }
--- src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/Vuelo/VuelosRepository.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/Vuelo/VuelosRepository.java
@@ -2,7 +2,7 @@ package uy.edu.um.proyecto.proyectotic.Persistencia.Vuelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface VuelosRepository extends JpaRepository<Vuelos,String>{
+public interface VuelosRepository extends JpaRepository<Vuelos,Long>{
     
     
 }
