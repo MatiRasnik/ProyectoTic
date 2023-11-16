3be22e23437a3ec90b4163f791205895b5bdb6f6
MatiRasnik <64052131+MatiRasnik@users.noreply.github.com>
2023-11-16 13:04:00 -0300

    cosas

--- src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/Vuelo/VuelosRepository.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/Persistencia/Vuelo/VuelosRepository.java
@@ -1,13 +1,15 @@
 package uy.edu.um.proyecto.proyectotic.Persistencia.Vuelo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-
-
-public interface VuelosRepository extends JpaRepository<Vuelos,Long>{
+public interface VuelosRepository extends JpaRepository<Vuelos, Long> {
     Vuelos findByCodigoVuelo(String codigoVuelo);
+
     List<Vuelos> findByAeropuertoLlegada(String aeropuertoLlegada);
+
     List<Vuelos> findByAeropuertoSalida(String aeropuertoLlegada);
-    
-    
+
+    List<Vuelos> findByAerolinea(String nombreEmpresa);
+
 }
--- src/main/java/uy/edu/um/proyecto/proyectotic/Servicios/RestServices/VuelosRestService.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/Servicios/RestServices/VuelosRestService.java
@@ -1,15 +1,23 @@
 package uy.edu.um.proyecto.proyectotic.Servicios.RestServices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import uy.edu.um.AvionesDTO;
 import uy.edu.um.VuelosDTO;
 import uy.edu.um.proyecto.proyectotic.Mappers.VuelosMapper;
 import uy.edu.um.proyecto.proyectotic.Servicios.VuelosService;
+import uy.edu.um.proyecto.proyectotic.Persistencia.Aviones.Aviones;
 import uy.edu.um.proyecto.proyectotic.Persistencia.Vuelo.Vuelos;
+import uy.edu.um.proyecto.proyectotic.Persistencia.Vuelo.VuelosRepository;
 
 @RestController
 public class VuelosRestService {
@@ -17,13 +25,34 @@ public class VuelosRestService {
     private VuelosMapper vuelosMapper;
     @Autowired
     private VuelosService vuelosService;
-
+    @Autowired
+    private VuelosRepository vuelosRepository;
 
     @PostMapping("/crearVuelo")
-    public void crearVuelo(@RequestBody VuelosDTO vuelosDTO) throws Exception{
-        
+    public void crearVuelo(@RequestBody VuelosDTO vuelosDTO) throws Exception {
+
         Vuelos vuelo = vuelosMapper.toVuelos(vuelosDTO);
         vuelosService.crearVuelo(vuelo);
+    }
+
+    @GetMapping("/getVuelos/{empresa}")
+    public List<VuelosDTO> getVuelos(@PathVariable("empresa") String nombreEmpresa) {
+        List<Vuelos> vuelosSalida = vuelosRepository.findByAeropuertoSalida(nombreEmpresa);
+        List<Vuelos> vuelosLlegada = vuelosRepository.findByAeropuertoLlegada(nombreEmpresa);
+
+        System.out.println(nombreEmpresa);
+        System.out.println(vuelosLlegada);
+        System.out.println(vuelosSalida);
+
+        List<VuelosDTO> vuelosDTOs = new ArrayList<>();
+        for (Vuelos avion : vuelosSalida) {
+            vuelosDTOs.add(vuelosMapper.toDTO(avion));
+        }
+        for (Vuelos avion : vuelosLlegada) {
+            vuelosDTOs.add(vuelosMapper.toDTO(avion));
+        }
+        System.out.println(vuelosDTOs);
+        return vuelosDTOs;
     }
 
 }
