0d22399ade39dc7782333dd36f2f3567e944d393
MatiRasnik <64052131+MatiRasnik@users.noreply.github.com>
2023-11-18 18:28:17 -0300

    empleados

--- src/main/java/uy/edu/um/proyecto/proyectotic/Servicios/RestServices/UsuariosRestService.java
+++ src/main/java/uy/edu/um/proyecto/proyectotic/Servicios/RestServices/UsuariosRestService.java
@@ -1,5 +1,8 @@
 package uy.edu.um.proyecto.proyectotic.Servicios.RestServices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import uy.edu.um.UsuariosDTO;
+import uy.edu.um.AvionesDTO;
 import uy.edu.um.UsuarioTransporte;
 import uy.edu.um.proyecto.proyectotic.Mappers.UsuariosMapper;
+import uy.edu.um.proyecto.proyectotic.Persistencia.Aviones.Aviones;
 import uy.edu.um.proyecto.proyectotic.Persistencia.Usuarios.Usuarios;
+import uy.edu.um.proyecto.proyectotic.Persistencia.Usuarios.UsuariosRepository;
 import uy.edu.um.proyecto.proyectotic.Servicios.UsuariosService;
 
 @RestController
@@ -21,30 +27,40 @@ public class UsuariosRestService {
     private UsuariosMapper usuariosMapper;
     @Autowired
     private UsuariosService usuariosService;
-    
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
     @PostMapping("/addUsuarios")
-    public void crearUsuario(@RequestBody UsuarioTransporte usuarioTransporte) throws Exception{
+    public void crearUsuario(@RequestBody UsuarioTransporte usuarioTransporte) throws Exception {
         Usuarios usuario = usuariosMapper.toUsuarios(usuarioTransporte.getUsuariosDTO());
         usuariosService.crearUsuario(usuario, usuarioTransporte.getLicenciaPiloto());
     }
 
     @GetMapping("/getUsuarios/{id}")
-    public UsuariosDTO obtenerUsuario(@PathVariable("id") String email) throws Exception{
-        Usuarios usr=usuariosService.getUsuarioId(email);
-        UsuariosDTO usrDTO= usuariosMapper.toDTO(usr);
+    public UsuariosDTO obtenerUsuario(@PathVariable("id") String email) throws Exception {
+        Usuarios usr = usuariosService.getUsuarioId(email);
+        UsuariosDTO usrDTO = usuariosMapper.toDTO(usr);
         return usrDTO;
     }
 
     @PostMapping("/verificarLogin")
-    public Boolean verificarLogin(@RequestBody UsuariosDTO usr) throws Exception{
+    public Boolean verificarLogin(@RequestBody UsuariosDTO usr) throws Exception {
         return usuariosService.verificarLogin(usr.getEmail(), usr.getContrasena());
-        
+
     }
 
     @PostMapping("/eliminarUsuario")
-    public void eliminarUsuario(@RequestBody String id) throws Exception{
+    public void eliminarUsuario(@RequestBody String id) throws Exception {
         usuariosService.eliminarUsuario(id);
     }
 
-
+    @GetMapping("/getEmpleados/{empresa}")
+    public List<UsuariosDTO> getEmpleados(@PathVariable("empresa") String nombreEmpresa) {
+        List<Usuarios> empleados = usuariosRepository.findByEmpresa(nombreEmpresa);
+        List<UsuariosDTO> empleadosDTO = new ArrayList<>();
+        for (Usuarios empleado : empleados) {
+            empleadosDTO.add(usuariosMapper.toDTO(empleado));
+        }
+        return empleadosDTO;
+    }
 }
